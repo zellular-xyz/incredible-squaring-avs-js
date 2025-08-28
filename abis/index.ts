53e6875eec8c3ade203f8565a700991351cf2e0f
sadegh <sadeghte@gmail.com>
2025-08-28 10:10:08 +0330

    bug fix

---
+++ abis/index.ts
@@ -0,0 +1,7 @@
+import SERVICE_MANAGER_ABI from './IncredibleSquaringServiceManager.json';
+import TASK_MANAGER_ABI from './IncredibleSquaringTaskManager.json'
+
+export {
+    SERVICE_MANAGER_ABI,
+    TASK_MANAGER_ABI
+}
